a8c98ab00a2b1bc71be9d5322e68469970d36cd5
Mani Maghsoudlou <manidlou@gmail.com>
2017-04-21 13:48:48 -0700

    fix bug: ignore with glob pattern (closes #1), add filter option

--- klaw-sync.js
+++ klaw-sync.js
@@ -1,60 +1,54 @@
 'use strict'
-var path = require('path')
-var mm = require('micromatch')
-var fs
+const path = require('path')
+const mm = require('micromatch')
+let fs
 try {
   fs = require('graceful-fs')
 } catch (e) {
   fs = require('fs')
 }
 
-function _procPath (dir, pathItem, opts, list) {
-  var nestedPath
-  var stat
-  // here since dir already resolved, we use string concatenation
-  // which showed faster performance than path.join() and path.resolve()
-  if (path.sep === '/') {
-    nestedPath = dir + '/' + pathItem
-  } else {
-    nestedPath = dir + '\\' + pathItem
-  }
-  stat = fs.lstatSync(nestedPath)
-  if (stat.isDirectory()) {
-    if (!opts.nodir) {
-      list.push({path: nestedPath, stats: stat})
-    }
-    list = walkSync(nestedPath, opts, list)
-  } else {
-    if (!opts.nofile) {
-      list.push({path: nestedPath, stats: stat})
-    }
-  }
-}
-
-function walkSync (dir, opts, list) {
-  var files
-  var ignore = []
-  opts = opts || {}
-  list = list || []
-  dir = path.resolve(dir)
-  try {
-    files = fs.readdirSync(dir)
-    if (opts.ignore) {
-      ignore = mm(files, opts.ignore)
-    }
-  } catch (er) {
-    throw er
-  }
-
-  for (var i = 0; i < files.length; i += 1) {
-    var file = files[i]
-    if (ignore.length > 0) {
-      if (ignore.indexOf(file) === -1) _procPath(dir, file, opts, list)
+function klawSync (dir, opts, ls) {
+  function procPath (pathItem) {
+    const stat = fs.lstatSync(pathItem)
+    const item = {path: pathItem, stats: stat}
+    if (stat.isDirectory()) {
+      if (!opts.nodir) {
+        if (opts.ignore) {
+          if (mm(pathItem, opts.ignore).length === 0) ls.push(item)
+        } else if (opts.filter) {
+          if (opts.filter(item)) ls.push(item)
+        } else {
+          ls.push(item)
+        }
+      }
+      ls = klawSync(pathItem, opts, ls)
     } else {
-      _procPath(dir, file, opts, list)
+      if (!opts.nofile) {
+        if (opts.ignore) {
+          if (mm(pathItem, opts.ignore).length === 0) ls.push(item)
+        } else if (opts.filter) {
+          if (opts.filter(item)) ls.push(item)
+        } else {
+          ls.push(item)
+        }
+      }
     }
   }
-  return list
+
+  opts = opts || {}
+  ls = ls || []
+  dir = path.resolve(dir)
+  const files = fs.readdirSync(dir)
+  for (let i = 0; i < files.length; i += 1) {
+    // here dir already resolved, we use string concatenation since
+    // showed better performance than path.join() and path.resolve()
+    let pathItem
+    if (path.sep === '/') pathItem = dir + '/' + files[i]
+    else pathItem = dir + '\\' + files[i]
+    procPath(pathItem)
+  }
+  return ls
 }
 
-module.exports = walkSync
+module.exports = klawSync
