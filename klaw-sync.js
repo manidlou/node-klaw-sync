f340803d804a04029c7335b3072b4da9ac69602e
Mani Maghsoudlou <manidlou@gmail.com>
2017-12-03 15:47:29 -0800

    refactor to have a consistent function signature internally

--- klaw-sync.js
+++ klaw-sync.js
@@ -3,28 +3,29 @@
 const fs = require('graceful-fs')
 const path = require('path')
 
-function klawSync (dir, opts, ls) {
-  if (!ls) {
-    opts = opts || {}
-    ls = []
-    dir = path.resolve(dir)
-  }
+function klawSync (dir, opts) {
+  dir = path.resolve(dir)
+  opts = opts || {}
+  return _klawSync(dir, opts, [])
+}
+
+function _klawSync (dir, opts, ls) {
   const paths = fs.readdirSync(dir).map(p => dir + path.sep + p)
   for (var i = 0; i < paths.length; i += 1) {
     const pi = paths[i]
-    const st = fs.lstatSync(pi)
+    const st = fs.statSync(pi)
     const item = {path: pi, stats: st}
     if (st.isDirectory()) {
       if (opts.filter) {
         if (opts.filter(item) && !opts.nodir) {
           ls.push(item)
-          ls = klawSync(pi, opts, ls)
+          ls = _klawSync(pi, opts, ls)
         } else {
-          if (!opts.noRecurseOnFailedFilter) ls = klawSync(pi, opts, ls)
+          if (!opts.noRecurseOnFailedFilter) ls = _klawSync(pi, opts, ls)
         }
       } else {
         if (!opts.nodir) ls.push(item)
-        ls = klawSync(pi, opts, ls)
+        ls = _klawSync(pi, opts, ls)
       }
     } else {
       if (opts.filter) {
