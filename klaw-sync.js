b337e077be79a29cfa99f66b40450cab0f5c35e6
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-05-20 00:16:32 +0800

    build(deps): bump dev dependencies to address (most) vulnerabilities

--- klaw-sync.js
+++ klaw-sync.js
@@ -11,10 +11,10 @@ function klawSync (dir, opts, ls) {
     if (opts.depthLimit > -1) opts.rootDepth = dir.split(path.sep).length + 1
   }
   const paths = opts.fs.readdirSync(dir).map(p => dir + path.sep + p)
-  for (var i = 0; i < paths.length; i += 1) {
+  for (let i = 0; i < paths.length; i += 1) {
     const pi = paths[i]
     const st = opts.fs.lstatSync(pi)
-    const item = {path: pi, stats: st}
+    const item = { path: pi, stats: st }
     const isUnderDepthLimit = (!opts.rootDepth || pi.split(path.sep).length - opts.rootDepth < opts.depthLimit)
     const filterResult = opts.filter ? opts.filter(item) : true
     const isDir = st.isDirectory()
